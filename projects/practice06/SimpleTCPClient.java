7399de8029caf2b0ccb3fe4384b063713281d140
SashaBabichev <35573361+BabichevAlex@users.noreply.github.com>
2020-12-08 18:12:56 +0200

    Practice06

---
+++ projects/practice06/SimpleTCPClient.java
@@ -0,0 +1,30 @@
+﻿package practice06;
+import java.io.*;
+import java.net.*;
+
+public class SimpleTCPClient {
+    public static void main(String[] args) {
+        Socket s = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        String serverAddr = "localhost";
+        int port = 8030;
+        try {
+            s = new Socket(serverAddr,port);
+            isr = new InputStreamReader(s.getInputStream());
+            br = new BufferedReader(isr);
+            String msg = br.readLine();
+            System.out.println("Повідомлення: "+msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                br.close();
+                isr.close();
+                s.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+}
